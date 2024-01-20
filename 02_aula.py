1fd74888bd60c28a55874428be43e63a550aef14
felipsbreno <brendo.filipe2050@gmail.com>
2024-01-20 20:20:13 -0300

    #master WIP

--- 02_aula.py
+++ 02_aula.py
@@ -9,32 +9,30 @@ from selenium.webdriver.common.keys import Keys
 
 from time import sleep
 
-url_google_maps = "https://www.google.com/maps"
-
 service = Service()
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 driver.implicitly_wait(2)
-driver.get(url_google_maps)
+driver.get("https://www.google.com/maps")
 
-def adiciona_destino(endereco):
-    barra_vazia = driver.find_element(By.ID, 'searchboxinput')
-    barra_vazia.clear()
-    barra_vazia.send_keys(endereco)
-    barra_vazia.send_keys(Keys.RETURN)
+def add_destiny(address):
+    inputSerachAddress = driver.find_element(By.ID, 'searchboxinput')
+    inputSerachAddress.clear()
+    inputSerachAddress.send_keys(address)
+    inputSerachAddress.send_keys(Keys.RETURN)
 
-def abre_rotas():
+def open_routes():
     xpath = "//button[@data-value='Rotas']"
     wait = WebDriverWait(driver, timeout=5)
-    botao_rotas = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
-    botao_rotas.click()
+    btn_routes = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+    btn_routes.click()
 
     xpath = "//button[@aria-label='Fechar rotas']"
     wait = WebDriverWait(driver, timeout=5)
-    botao_rotas = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+    btn_routes = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
 
 if __name__ == '__main__':
-    endereco = 'Av. José Bonifácio, 245 - Farroupilha, Porto Alegre - RS, 90040-130' # Redenção
-    adiciona_destino(endereco=endereco)
-    abre_rotas()
+    address = 'Av. José Bonifácio, 245 - Farroupilha, Porto Alegre - RS, 90040-130' # Redenção
+    add_destiny(address=address)
+    open_routes()
     sleep(600)    
