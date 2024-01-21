52f7a58efbc4a8e61969542bb174fb8b683f063c
felipsbreno <brendo.filipe2050@gmail.com>
2024-01-21 00:27:47 -0300

    create optimize routes and get the time total and the distance too

--- 02_aula.py
+++ 02_aula.py
@@ -16,6 +16,9 @@ driver.implicitly_wait(2)
 driver.maximize_window()
 driver.get("https://www.google.com/maps")
 
+def sleep_before_interactive(time = 2):
+    return sleep(time)
+
 def Its_in_the_routes_tab():
     xpath = '//button[@aria-label="Fechar rotas"]'
     btn_routes = driver.find_elements(By.XPATH, xpath)
@@ -52,6 +55,53 @@ def open_routes():
     wait = WebDriverWait(driver, timeout=5)
     btn_routes = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
 
+def add_more_destiny():
+    xpath = '//*[@id="omnibox-directions"]/div/div[3]/button/div[2]/span'
+    wait = WebDriverWait(driver, timeout=3)
+    wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
+
+    btn_add_more_destiny = driver.find_element(By.XPATH, xpath)
+    btn_add_more_destiny.click()
+
+def select_type_condution(type_condution = 'Carro'):
+    xpath = f'//img[@aria-label="{type_condution}"]'
+    wait = WebDriverWait(driver, timeout=3)
+    btn_select_type_condution = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+    btn_select_type_condution.click()
+
+def return_time_total():
+    xpath = '//*[@id="section-directions-trip-0"]//div[contains(text(), "min")]'
+    wait = WebDriverWait(driver, timeout=3)
+    time_element = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+    return int(time_element.text.replace(' min', ''))
+
+def return_distance_total():
+    xpath = '//*[@id="section-directions-trip-0"]//div[contains(text(), "km")]'
+    wait = WebDriverWait(driver, timeout=3)
+    time_element = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+    return float(time_element.text.replace(' km', '').replace(',', '.'))
+
+
+# =========================== Funções Principais ===============================================
+
+def generate_pares_distance(address):
+    distance_pares = {}
+    driver.get("https://www.google.com/maps")
+    add_destiny(address[0], 1)
+    open_routes()
+    select_type_condution(type_condution="Carro")
+
+    for i, end1 in enumerate(address):
+        add_destiny(end1, 1)
+        for j, end2 in enumerate(address):
+            if i != j:
+                add_destiny(end2, 2)
+                time_par = return_time_total()
+                distance_pares[f'{i}_{j}'] = time_par
+
+    return distance_pares
+
+
 if __name__ == '__main__':
     address = [
                 'Av. José Bonifácio, 245 - Farroupilha, Porto Alegre - RS, 90040-130', # Redenção
@@ -60,12 +110,6 @@ if __name__ == '__main__':
                 'Av. Padre Cacique, 2000 - Praia de Belas, Porto Alegre - RS, 90810-180', #Iberê
               ]
     
-    add_destiny(address[0], 1)
-    open_routes()
-
-    add_destiny(address[0], 1)
-    add_destiny(address[1], 2)
-    add_destiny(address[2], 3)
-    add_destiny(address[3], 4)
-    
-    sleep(600)    
+    distance_pares = generate_pares_distance(address)
+    print(distance_pares)
+    sleep_before_interactive(time=600)    
